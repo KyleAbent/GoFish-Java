d35835b77f8f3eac223f8e4ab4f73afd00f70fb5
KyleAbent <kyleabent@gmail.com>
2020-01-02 12:30:48 -0800

    1.20.20

--- GoFish.java
+++ GoFish.java
@@ -238,7 +238,6 @@ public class GoFish  extends JFrame implements ActionListener
       }
       else if (e.getSource() == endTurn)
       {
-         System.out.println("dsds New Game Selected");
          updateGUI();
          fEngine.setScanNumber(0);
          fEngine.humanAlgorithm();
@@ -248,7 +247,6 @@ public class GoFish  extends JFrame implements ActionListener
       }
       else if (e.getSource() == newGame)
       {
-         System.out.println("New Game Selected");
          //fEngine.turnIgnition(); 
          //fishEngine fEngine = new fishEngine();
          //fEngine.turnIgnition();
